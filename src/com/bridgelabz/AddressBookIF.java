5761f0b3168ef0f35d325f29157b3cef3fc3e0c9
HemnathG <hemnathdavid@gmail.com>
2022-04-21 18:19:49 +0530

    Adding fileIO

---
+++ src/com/bridgelabz/AddressBookIF.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+import java.util.List;
+import java.util.Scanner;
+
+import com.bridgelabz.AddressBook.IOService;
+
+
+public interface AddressBookIF {
+
+    public void operation();
+
+    public void createContactPerson(Scanner scannerObject);
+
+    public void addContact(String firstName, ContactPerson person);
+
+    public void displayContents();
+
+    public void editPerson();
+
+    public void deletePerson();
+
+    public void addPersonToCity(ContactPerson contact);
+
+    public void addPersonToState(ContactPerson contact);
+
+    public void printSortedList(List<ContactPerson> sortedContactList);
+
+    public void sortAddressBook(int sortingChoice);
+
+    public void writeToAddressBookFile(IOService ioService);
+
+    public List<String> readDataFromFile(IOService fileIo);
+}
